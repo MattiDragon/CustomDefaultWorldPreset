abc73236ec312a0dcb086a86b77ed623b80464ee
MattiDragon <matias.koivikko@gmail.com>
2025-12-25 21:22:12 +0200

    Add mc version override env vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ publishMods {
         accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
 
         requires("fabric-api")
-        minecraftVersions.add(mcVersion)
+        minecraftVersions.add(providers.environmentVariable("MODRINTH_MC_VERSION").orElse(mcVersion))
     }
 
     curseforge {
@@ -101,7 +101,7 @@ publishMods {
         accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
 
         requires("fabric-api")
-        minecraftVersions.add(mcVersion)
+        minecraftVersions.add(providers.environmentVariable("CURSEFORGE_MC_VERSION").orElse(mcVersion))
     }
 
     github {
